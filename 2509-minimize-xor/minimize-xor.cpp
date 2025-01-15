2ccb16176690e61802357213dbc3f0de8e8aaa40
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-01-15 21:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (64.02%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -14,15 +14,15 @@ public:
         // Use the most significant bits of num1 to set bits in result
         for (int i = 31; i >= 0 && cnt > 0; i--) {
             if (num1 & (1 << i)) {
-                result |= (1 << i);
+                result += (1 << i);
                 cnt--;
             }
         }
 
         // If there are still bits to set, set the least significant bits
         for (int i = 0; i <= 31 && cnt > 0; i++) {
-            if (!(result & (1 << i))) {
-                result |= (1 << i);
+            if (!(num1 & (1 << i))) {
+                result += (1 << i);
                 cnt--;
             }
         }
