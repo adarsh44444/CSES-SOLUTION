f3eddcb049582d7739a8299bf1a8b856745c4bee
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-05-19 21:07:20 +0530

    Time: 4 ms (15.25%) | Memory: 17.6 MB (13.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>> &grid,int n,int m,vector<vector<int>> &dp){
-        if(i==n-1&&j==m-1) return grid[n-1][m-1];
-        int moveright=1e9,movedown=1e9;
+    int solve(int i,int j,vector<vector<int>> &grid,vector<vector<int>>&dp){
+        int n=grid.size();
+        int m=grid[0].size();
+        if(i==n||j==m||i==-1||j==-1) return 1e9;
+        if(i==n-1&&j==m-1) return grid[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        if(i+1<n){
-            moveright=grid[i][j]+solve(i+1,j,grid,n,m,dp);
-        }
-        if(j+1<m){
-            movedown=grid[i][j]+solve(i,j+1,grid,n,m,dp);
-        }
-        return dp[i][j]=min(moveright,movedown);
+        int ans=1e9;
+        int val=grid[i][j];
+        ans=min({val+solve(i+1,j,grid,dp),val+solve(i,j+1,grid,dp)});
+        return dp[i][j]=ans;
     }
     int minPathSum(vector<vector<int>>& grid) {
+        // dynamic programming solution will work not the greedy one
+        // memoization stuff
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(0,0,grid,n,m,dp);
+        return solve(0,0,grid,dp);
     }
 };
