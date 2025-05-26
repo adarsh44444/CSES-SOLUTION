8ba58cb27e10910d20dc42e69bbe053c84a4b571
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-05-26 15:27:05 +0530

    Time: 188 ms (14.52%) | Memory: 204.1 MB (6.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-bool check(int index,int sum,vector<int>& nums,vector<vector<int>> &dp){
-    if(index==0) return nums[index]==sum;
-    bool pick=false,notpick;
-    if(dp[index][sum]!=-1) return dp[index][sum];
-    notpick=check(index-1,sum,nums,dp);
-    if(nums[index]<=sum){
-    pick=check(index-1,sum-nums[index],nums,dp);}
-    return dp[index][sum]=pick|notpick;
-}
-    bool canPartition(vector<int>& nums){
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0) return false;
-        vector<vector<int>> dp(nums.size()+1,vector<int>(sum/2+1,-1));
-        return check(nums.size()-1,sum/2,nums,dp);
+    bool solve(int index,vector<int> &nums,int target,vector<vector<int>> &dp){
+        if(index==nums.size()-1) return nums[index]==target;
+        if(target==0) return 1;
+        bool ans=false;
+        if(dp[index][target]!=-1) return dp[index][target];
+        if(target-nums[index]>=0){
+        ans|=solve(index+1,nums,target-nums[index],dp);}
+        ans|=solve(index+1,nums,target,dp);
+        return dp[index][target]=ans;
+    }
+    bool canPartition(vector<int>& nums) {
+        // main logic is can we divide it into two equal halfes
+        long long sum=accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size();
+        if(sum%2!=0) return 0;
+        vector<vector<int>>dp(n,vector<int>(sum,-1));
+        return solve(0,nums,sum/2,dp);
     }
 };
