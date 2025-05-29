1f2fdd9f3c01e72c73e5c21e52492cc71a558198
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-05-29 14:37:43 +0530

    Time: 79 ms (8.99%) | Memory: 54 MB (9.33%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -16,10 +16,30 @@ int solve(int index,vector<int> &coins,int amount){
     return dp[index][amount]=ans;
 }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        int n=coins.size();
-        int ans=solve(n-1,coins,amount);
-        if(ans==1e9) return -1;
-        return ans;
+    int n = coins.size();
+    vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
+
+    // Base case: using only the first coin
+    for (int j = 0; j <= amount; j++) {
+        if (j % coins[0] == 0)
+            dp[0][j] = j / coins[0];
     }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j <= amount; j++) {
+            // Not pick the coin
+            int notPick = dp[i - 1][j];
+
+            // Pick the coin (could be the same coin again)
+            int pick = 1e9;
+            if (coins[i] <= j)
+                pick =min(1+dp[i-1][j-coins[i]], 1 + dp[i][j - coins[i]]);
+
+            dp[i][j] = min(pick, notPick);
+        }
+    }
+
+    return dp[n - 1][amount] >= 1e9 ? -1 : dp[n - 1][amount];
+}
+
 };
