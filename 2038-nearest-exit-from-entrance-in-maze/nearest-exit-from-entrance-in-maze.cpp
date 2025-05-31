8663b4a58fed97664ba0de97701e61557da0e89f
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-05-31 17:55:11 +0530

    Time: 16 ms (41.05%) | Memory: 36.4 MB (54.22%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -3,29 +3,29 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         pq.push({0,{entrance[0],entrance[1]}});
-        int m=maze.size();
-        int n=maze[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,-1));
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+        int n=maze.size(),m=maze[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
         vis[entrance[0]][entrance[1]]=1;
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
+        int u=entrance[0];
+        int v=entrance[1];
         while(!pq.empty()){
-            int dist=pq.top().first;
+            int cnt=pq.top().first;
             int row=pq.top().second.first;
             int col=pq.top().second.second;
-            if((row==m-1||row==0||col==n-1||col==0)&&(row!=entrance[0]||col!=entrance[1])) return dist;
+            if(row==u&&col==v){}
+            else if(row==n-1||row==0||col==0||col==m-1) return cnt;
             pq.pop();
             for(int i=0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
-                if(nrow<m&&nrow>=0&&ncol>=0&&ncol<n){
-                if(maze[nrow][ncol]=='.'&&vis[nrow][ncol]==-1){
-                    pq.push({dist+1,{nrow,ncol}});
+                int nrow=row+dx[i];
+                int ncol=col+dy[i];
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&vis[nrow][ncol]==0&&maze[nrow][ncol]=='.'){
                     vis[nrow][ncol]=1;
+                    pq.push({cnt+1,{nrow,ncol}});
                 }
             }
-
         }
+        return -1;
     }
-    return -1;}
 };
