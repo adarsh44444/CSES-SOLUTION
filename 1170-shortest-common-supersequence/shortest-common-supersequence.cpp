0671575520c9c1c21e28d1ad2a3ca915aca9d854
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-06-04 12:04:37 +0530

    Time: 17 ms (97.23%) | Memory: 13 MB (97.77%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
+int dp[1001][1001];
     string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        memset(dp,0,sizeof(dp));
+        int n=str1.size(),m=str2.size();
         for(int i=0;i<=n;i++) dp[i][0]=0;
         for(int j=0;j<=m;j++) dp[0][j]=0;
         for(int i=1;i<=n;i++){
@@ -12,40 +12,21 @@ public:
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        int i1=n;
-        string ans="";
-        int j1=m;
-        while(i1>0&&j1>0){
-            if(str1[i1-1]==str2[j1-1]){
-                ans+=str1[i1-1];
-                i1--;
-                j1--;
-            }
-            else if(dp[i1-1][j1]>dp[i1][j1-1]){
-                ans+=str1[i1-1];
-                i1--;
-            }
-            else{
-                ans+=str2[j1-1];
-                j1--;
-            }
+    }
+    string ans="";
+    int i=n,j=m;
+    while(i>0&&j>0){
+        if(str1[i-1]==str2[j-1]) {ans+=str1[i-1];i--;j--;}
+        else if(dp[i-1][j]>dp[i][j-1]) {ans+=str1[i-1]; i--;}
+        else {ans+=str2[j-1]; j--;}
+    }
+    while(i>0) {ans+=str1[i-1]; i--;}
+    while(j>0){ans+=str2[j-1];j--;}
+    reverse(ans.begin(),ans.end());
+    return ans;
 
-            }
-            while(i1>0){
-                ans+=str1[i1-1];
-                i1--;
-            }
-            while(j1>0){
-                ans+=str2[j1-1];
-                j1--;
-            }
-            reverse(ans.begin(),ans.end());
-            return ans;
-        }
-
-    
+    }
 };
