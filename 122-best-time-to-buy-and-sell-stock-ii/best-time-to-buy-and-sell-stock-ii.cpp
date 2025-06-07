22bd7746f9aaf42922d08af0607194c6673a5401
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-06-07 20:41:30 +0530

    Time: 9 ms (7.42%) | Memory: 18 MB (32.78%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,6 +17,23 @@ int solve(int index,vector<int>& prices,int buy){
 }
     int maxProfit(vector<int>& prices) {
         memset(dp,-1,sizeof(dp));
-        return solve(0,prices,0);
+        int n=prices.size();
+        dp[n][0]=0;
+        dp[n][1]=0;
+    
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                int ans=0;
+                if(j==0){
+                    ans=max({ans,-prices[i]+dp[i+1][1],dp[i+1][0]});
+                }
+                else{
+                    ans=max({ans,prices[i]+dp[i+1][0],dp[i+1][1]});
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+        /* return solve(0,prices,0); */
     }
 };
