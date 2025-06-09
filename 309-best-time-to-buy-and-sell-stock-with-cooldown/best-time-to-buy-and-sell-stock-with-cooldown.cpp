f33b47c0d1a3c81752a3f52a5148751be5268245
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-06-09 20:11:12 +0530

    Time: 3 ms (34.94%) | Memory: 15.8 MB (70.19%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-int fun(int index,vector<int>& prices,int n,int buy,vector<vector<int>> &dp){
-    if(index>=n) return 0;
-    int pick,notpick,sell;
-    if(dp[index][buy]!=-1) return dp[index][buy];
-    if(buy==0){
-        dp[index][buy]=max(-prices[index]+fun(index+1,prices,n,!buy,dp),fun(index+1,prices,n,buy,dp));
+    int dp[5001][2];
+    int solve(int index,vector<int> &prices,int n,int buy){
+        if(index>=n) return 0;
+        int ans=0;
+        if(dp[index][buy]!=-1) return dp[index][buy];
+        if(buy==0){
+            ans=max({ans,-prices[index]+solve(index+1,prices,n,1),solve(index+1,prices,n,0)});
+        }
+        else{
+            ans=max({ans,prices[index]+solve(index+2,prices,n,0),solve(index+1,prices,n,1)});
+        }
+        return dp[index][buy]=ans;
     }
-    else{
-        dp[index][buy]=max(prices[index]+fun(index+2,prices,n,!buy,dp),fun(index+1,prices,n,buy,dp));
-    }
-    return dp[index][buy];
-    
-}
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(3,-1));
-        return fun(0,prices,n,0,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(0,prices,n,0);
     }
 };
