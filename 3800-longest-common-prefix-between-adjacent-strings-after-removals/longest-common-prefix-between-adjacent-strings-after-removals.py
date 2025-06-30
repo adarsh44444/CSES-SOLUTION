25784b7360817025e5f3adfe93da579bcc6e6f1d
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-06-30 23:48:37 +0530

    Time: 867 ms (77.25%) | Memory: 48.9 MB (5.92%) - LeetSync

--- 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.py
+++ 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.py
@@ -1,57 +1,51 @@
 class Solution:
-    def getLCP(self, i: int, j: int, words: list[str]) -> int:
-        l = 0
-        w1, w2 = words[i], words[j]
-        while l < len(w1) and l < len(w2) and w1[l] == w2[l]:
-            l += 1
-        return l
+    def _common_prefix_len(self, a: str, b: str) -> int:
+        i = 0
+        while i < len(a) and i < len(b) and a[i] == b[i]:
+            i += 1
+        return i
 
     def longestCommonPrefix(self, words: list[str]) -> list[int]:
-        n = len(words)
+        sz = len(words)
+        if sz == 1: return [0]
+        if sz == 2: return [0, 0]
+
+        pair_scores = []
+        idx = 1
+        limit = sz - 1 if sz % 2 else sz - 2
+
+        while idx < limit:
+            pair_scores.append((self._common_prefix_len(words[idx - 1], words[idx]), (idx - 1, idx)))
+            pair_scores.append((self._common_prefix_len(words[idx], words[idx + 1]), (idx, idx + 1)))
+            idx += 2
+
+        (pair_scores.append((self._common_prefix_len(words[sz - 2], words[sz - 1]), (sz - 2, sz - 1)))
+         if sz % 2 == 0 else None)
+
+        pair_scores.sort(reverse=True)
+
         result = []
+        first_pair = pair_scores[0]
+        second_pair = pair_scores[1] if len(pair_scores) > 1 else (-1, (-1, -1))
 
-        if n == 1:
-            return [0]
-        elif n == 2:
-            return [0, 0]
-
-        lcpPairs = []
-        i = 1
-
-        if n % 2 != 0:
-            while i < n - 1:
-                lcpPairs.append((self.getLCP(i - 1, i, words), (i - 1, i)))
-                lcpPairs.append((self.getLCP(i, i + 1, words), (i, i + 1)))
-                i += 2
-        else:
-            while i < n - 2:
-                lcpPairs.append((self.getLCP(i - 1, i, words), (i - 1, i)))
-                lcpPairs.append((self.getLCP(i, i + 1, words), (i, i + 1)))
-                i += 2
-            lcpPairs.append((self.getLCP(n - 2, n - 1, words), (n - 2, n - 1)))
-
-        lcpPairs.sort(reverse=True)
-
-        result.append(lcpPairs[1][0] if len(lcpPairs) >= 2 and lcpPairs[0][1][0] == 0 else lcpPairs[0][0])
+        result.append(second_pair[0] if len(pair_scores) >= 2 and first_pair[1][0] == 0 else first_pair[0])
 
         mid = 1
-        while mid < n - 1:
-            center = self.getLCP(mid - 1, mid + 1, words)
-            a1, b1 = lcpPairs[0][1]
-            a2, b2 = lcpPairs[1][1] if len(lcpPairs) > 1 else (-1, -1)
+        while mid < sz - 1:
+            mid_score = self._common_prefix_len(words[mid - 1], words[mid + 1])
+            f1, f2 = first_pair[1]
+            s1, s2 = second_pair[1]
 
-            if center >= lcpPairs[0][0]:
-                result.append(center)
-            else:
-                if a1 != mid and b1 != mid:
-                    result.append(max(center, lcpPairs[0][0]))
-                elif a2 != mid and b2 != mid and len(lcpPairs) > 1:
-                    result.append(max(center, lcpPairs[1][0]))
-                elif len(lcpPairs) > 2:
-                    result.append(max(center, lcpPairs[2][0]))
-                else:
-                    result.append(max(center, 0))
+            result.append(
+                mid_score if mid_score >= first_pair[0]
+                else max(mid_score,
+                         first_pair[0] if f1 != mid and f2 != mid
+                         else second_pair[0] if s1 != mid and s2 != mid and len(pair_scores) > 1
+                         else pair_scores[2][0] if len(pair_scores) > 2
+                         else 0
+                )
+            )
             mid += 1
 
-        result.append(lcpPairs[1][0] if len(lcpPairs) >= 2 and lcpPairs[0][1][1] == n - 1 else lcpPairs[0][0])
+        result.append(second_pair[0] if len(pair_scores) >= 2 and first_pair[1][1] == sz - 1 else first_pair[0])
         return result
