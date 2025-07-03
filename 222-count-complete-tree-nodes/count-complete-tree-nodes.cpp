642c5d164471a9db7708f4334ec9f958df5b3938
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-03 22:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (43.93%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -1,37 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-int findleft(TreeNode* root){
-    int cnt=0;
-    while(root){
-        root=root->left;
-        cnt++;
+    int findleftheight(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root->left;
+        }
+        return height;
     }
-    return cnt;
-}
-int findright(TreeNode* root){
-    int cnt=0;
-    while(root){
-        root=root->right;
-        cnt++;
+
+    int findrightheight(TreeNode* root){
+        int height = 0;
+        while(root){
+            height++;
+            root = root->right;
+        }
+        return height;
     }
-    return cnt;
-}
+
     int countNodes(TreeNode* root) {
-        if(root==nullptr) return 0;
-        int lh=findleft(root);
-        int rh=findright(root);
-        if(lh==rh) return pow(2,lh)-1;
-        else return 1+countNodes(root->left)+countNodes(root->right);
+        if (root == nullptr) return 0;
+
+        int lh = findleftheight(root);
+        int rh = findrightheight(root);
+
+        if (lh == rh)
+            return (1 << lh) - 1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
