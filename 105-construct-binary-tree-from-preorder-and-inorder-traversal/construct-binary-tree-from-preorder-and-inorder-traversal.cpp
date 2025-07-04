cd0a636f52203ada0935ea4e9b81b661699b0f25
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-04 23:31:53 +0530

    Time: 2 ms (79.73%) | Memory: 27.4 MB (56.14%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,32 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* build(vector<int> &preorder,vector<int> &inorder,int pres,int pree,int ins,int ine,map<int,int> &mp){
-        if(pres>pree||ins>ine) return nullptr;
-        TreeNode* root=new TreeNode(preorder[pres]);
-        int index=mp[preorder[pres]];
-        int numsleft=index-ins;
-        root->left=build(preorder,inorder,pres+1,pres+numsleft,ins,index-1,mp);
-        root->right=build(preorder,inorder,pres+numsleft+1,pree,index+1,ine,mp);
+    TreeNode* solve(unordered_map<int, int> &mp, int &preindex, vector<int> &preorder, int left, int right) {
+        if (left > right) return nullptr;
+
+        int val = preorder[preindex++];
+        TreeNode* root = new TreeNode(val);
+
+        int idx = mp[val];
+
+        root->left = solve(mp, preindex, preorder, left, idx - 1);
+        root->right = solve(mp, preindex, preorder, idx + 1, right);
+
         return root;
     }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int> mp;
-        for(int i=0;i<inorder.size();i++){
-            mp[inorder[i]]=i;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
         }
-        int n=inorder.size();
-        TreeNode* root=build(preorder,inorder,0,n-1,0,n-1,mp);
-        return root;
+        int preindex = 0;
+        return solve(mp, preindex, preorder, 0, preorder.size() - 1);
     }
 };
