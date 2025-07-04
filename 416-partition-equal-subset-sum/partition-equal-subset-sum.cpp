a0f47e7bf07b73eb91b86e5c777453255d8bf35e
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-04 22:26:24 +0530

    Time: 524 ms (5.02%) | Memory: 48.5 MB (46.79%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool solve(int index,vector<int> &nums,int target,vector<vector<int>> &dp){
-        if(index==nums.size()-1) return nums[index]==target;
-        if(target==0) return 1;
-        bool ans=false;
-        if(dp[index][target]!=-1) return dp[index][target];
-        if(target-nums[index]>=0){
-        ans|=solve(index+1,nums,target-nums[index],dp);}
-        ans|=solve(index+1,nums,target,dp);
-        return dp[index][target]=ans;
+#define ll long long
+ll dp[201][20001];
+bool solve(int idx,vector<int> &nums,ll target){
+    if(idx==nums.size()) return target==0;
+    bool ans=false;
+    if(dp[idx][target]!=-1) return dp[idx][target];
+    ans|=solve(idx+1,nums,target);
+    if(target>=nums[idx]){
+        ans|=solve(idx+1,nums,target-nums[idx]);
     }
+    return dp[idx][target]=ans;
+}
     bool canPartition(vector<int>& nums) {
-        // main logic is can we divide it into two equal halfes
-        long long sum=accumulate(nums.begin(),nums.end(),0);
-        int n=nums.size();
-        if(sum%2!=0) return 0;
-        vector<vector<int>>dp(n,vector<int>(sum,-1));
-        return solve(0,nums,sum/2,dp);
+        memset(dp,-1,sizeof(dp));
+        ll sum=accumulate(nums.begin(),nums.end(),0ll);
+        if(sum%2!=0) return false;
+        return solve(0,nums,sum/2);
     }
 };
