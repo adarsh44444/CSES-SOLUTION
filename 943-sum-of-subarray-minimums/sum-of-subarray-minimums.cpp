4c1a9168440420c0d39a9875197bfb9f194e5596
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-06 23:20:02 +0530

    Time: 12 ms (84.06%) | Memory: 46.9 MB (87.77%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -7,30 +7,23 @@ public:
         int n = arr.size();
         vector<int> pse(n, -1), nse(n, n);
         stack<int> st;
-
-        // Find Next Smaller Element
         for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && arr[i] <= arr[st.top()]) st.pop();
             if (!st.empty()) nse[i] = st.top();
             st.push(i);
         }
-
         while (!st.empty()) st.pop();
-
-        // Find Previous Smaller Element
         for (int i = 0; i < n; i++) {
             while (!st.empty() && arr[i] < arr[st.top()]) st.pop();
             if (!st.empty()) pse[i] = st.top();
             st.push(i);
         }
-
         ll ans = 0;
         for (int i = 0; i < n; i++) {
             ll left = i - pse[i];
             ll right = nse[i] - i;
             ans = (ans + (1LL * arr[i] * left % mod * right % mod)) % mod;
         }
-
         return ans;
     }
 };
