71ae1aef4dd0c35113f6994801a35998499b6ae0
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-07 10:01:26 +0530

    Time: 986 ms (5.06%) | Memory: 11.7 MB (83.28%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int dp[21][100000];
-    int solve(int index,vector<int> &nums,int target){
-        if(index==0){
-            if(target+nums[index]==0&&target==nums[index]) return 2;
-            if(target+nums[index]==0||target==nums[index]) return 1;
-            return 0;
-        }
-        if(dp[index][target+10000]!=-1) return dp[index][target+10000];
-        int notpick=solve(index-1,nums,target+nums[index]);
-        int pick=solve(index-1,nums,target-nums[index]);
-        return dp[index][target+10000]=pick+notpick;
-    }
+int solve(int index,int sum,vector<int> &nums,int target){
+    if(index==nums.size()){
+        return sum==target;
+}
+    int add=0,sub=0;
+    add=solve(index+1,sum+nums[index],nums,target);
+    sub=solve(index+1,sum-nums[index],nums,target);
+    return add+sub;
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums.size()-1,nums,target);
+        return solve(0,0,nums,target);        
     }
 };
