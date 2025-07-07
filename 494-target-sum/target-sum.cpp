4c3dacdcaee96f14bfa648581e4a27e784926a14
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-07 10:06:04 +0530

    Time: 7 ms (69.92%) | Memory: 12 MB (74.23%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
+int dp[21][3000];
 int solve(int index,int sum,vector<int> &nums,int target){
     if(index==nums.size()){
         return sum==target;
-}
-    int add=0,sub=0;
+}   if(dp[index][sum+1000]!=-1) return dp[index][sum+1000];
+   int add=0,sub=0;
     add=solve(index+1,sum+nums[index],nums,target);
     sub=solve(index+1,sum-nums[index],nums,target);
-    return add+sub;
+    return dp[index][sum+1000]=add+sub;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
         return solve(0,0,nums,target);        
     }
 };
