a3df3a54d3c322f9acbf42893c96e574b3fbffab
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-10 09:19:43 +0530

    Time: 1 ms (82.01%) | Memory: 27.6 MB (38.39%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,30 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
-public:
-    TreeNode* build(int is,int ie,int ps,int pe,vector<int>& inorder,vector<int>& postorder,map<int,int>& mp){
-        if(is>ie||ps>pe) return nullptr;
-        TreeNode* root=new TreeNode(postorder[pe]);
-        int inroot=mp[postorder[pe]];
-        int numsleft=inroot-is;
-        root->left=build(is,inroot-1,ps,ps+numsleft-1,inorder,postorder,mp);
-        root->right=build(inroot+1,ie,ps+numsleft,pe-1,inorder,postorder,mp);
+    unordered_map<int, int> inorderIndex;
+    int postIndex;
+
+    TreeNode* build(vector<int>& inorder, vector<int>& postorder, int left, int right) {
+        if (left > right) return NULL;
+
+        int rootVal = postorder[postIndex--];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int idx = inorderIndex[rootVal];
+
+        // Build right subtree first
+        root->right = build(inorder, postorder, idx + 1, right);
+        root->left = build(inorder, postorder, left, idx - 1);
+
         return root;
     }
+
+public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int,int> mp;
-        for(int i=0;i<inorder.size();i++){
-            mp[inorder[i]]=i;
+        postIndex = postorder.size() - 1;
+        for (int i = 0; i < inorder.size(); ++i) {
+            inorderIndex[inorder[i]] = i;
         }
-        return build(0,inorder.size()-1,0,postorder.size()-1,inorder,postorder,mp);
+        return build(inorder, postorder, 0, inorder.size() - 1);
     }
 };
