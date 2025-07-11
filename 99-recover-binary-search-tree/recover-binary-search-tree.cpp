edc795481b0e4c4b069730f1ba6d841148a9e41b
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-11 09:43:33 +0530

    Time: 4 ms (12.99%) | Memory: 60.7 MB (96.21%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,36 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* first = nullptr;
     TreeNode* second = nullptr;
     TreeNode* prev = nullptr;
 
-    void recoverTree(TreeNode* root) {
-        inorderTraversal(root);
-        swap(first->val, second->val);
-    }
-
-    void inorderTraversal(TreeNode* root) {
+    void inorder(TreeNode* root) {
         if (!root) return;
-        
-        inorderTraversal(root->left);
 
-        if (prev && root->val < prev->val) {
+        inorder(root->left);
+
+        if (prev && prev->val > root->val) {
             if (!first) first = prev;
             second = root;
         }
         prev = root;
 
-        inorderTraversal(root->right);
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        if (first && second) {
+            swap(first->val, second->val);
+        }
     }
 };
