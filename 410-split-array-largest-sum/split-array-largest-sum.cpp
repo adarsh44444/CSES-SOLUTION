c2da59618bd8901466a669d3e707bfe8c43c912f
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-14 12:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (87.23%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-#define ll long long
+bool solve(int mid,vector<int> &nums,int k){
+    int n=nums.size();
+    int cnt=1;
+    int curr=0;
+    for(int i=0;i<n;i++){
+        if(curr+nums[i]<=mid){
+            curr+=nums[i];
+        }
+        else{
+            curr=nums[i];
+            cnt++;
+        }
+    }
+    return cnt<=k;
+}
     int splitArray(vector<int>& nums, int k) {
-        ll n=nums.size();
-        ll mid;
-        ll low=*max_element(nums.begin(),nums.end());
-        ll high=accumulate(nums.begin(),nums.end(),0ll);
+        int low=*max_element(nums.begin(),nums.end());
+        int high=1e9;
+        int mid;
+        int ans;
         while(low<=high){
             mid=(low+high)/2;
-            ll cnt=1;
-            ll temp=0;
-            for(int i=0;i<n;i++){
-                if(temp+nums[i]>mid){
-                    cnt++;
-                    temp=0;
-                }
-                temp+=nums[i];
-            }
-            if(cnt>k){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            if(solve(mid,nums,k)) {ans=mid; high=mid-1;}
+            else low=mid+1;
         }
-        return low;
+        return ans;
     }
 };
