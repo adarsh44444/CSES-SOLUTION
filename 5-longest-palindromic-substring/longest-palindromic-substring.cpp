087a7c59ae6ce8dc71aacdf0fd269051aa5b8163
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-16 12:03:03 +0530

    Time: 9 ms (72.73%) | Memory: 9.2 MB (92.92%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-bool fun(int s1, int e, string &s) {
-    int i = s1;
-    int j = e;
-    while (i < j) {
-        if (s[i] != s[j]) {
-            return false;  // Directly return false if mismatch is found
-        }
-        i++;
-        j--;
-    }
-    return true;
-}
-
-string longestPalindrome(string s) {
-    int n = s.size();
-    string str = "";
-    int maxi = 0;  // Use maxi to track the longest palindrome length
-
-    for (int i = 0; i < n; i++) {  // Check from i to n
-        for (int j = n - 1; j >= i; j--) {  // Check from the end to i
-            if (s[i] == s[j]) {
-                if (fun(i, j, s)) {
-                    if (j - i + 1 > maxi) {
-                        maxi = j - i + 1;  // Update maxi to the new length
-                        str = s.substr(i, maxi);  // Use j - i + 1 for the length
-                    }
+    string longestPalindrome(string s) {
+        int start=0;
+        int maxi=1;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            int l=i, r=i;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                if(maxi<r-l+1){
+                    maxi=r-l+1;
+                    start=l;
                 }
+                l--;
+                r++;
+            }
+            l=i,r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                if(maxi<r-l+1){
+                    maxi=r-l+1;
+                    start=l;
+                }
+                l--;
+                r++;
             }
         }
+        return s.substr(start,maxi);
     }
-    return str;
-}
-
 };
