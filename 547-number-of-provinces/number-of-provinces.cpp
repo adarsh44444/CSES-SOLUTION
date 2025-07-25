a1e09538910e4a6cc871138272cb859018ac5b5d
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-25 10:07:19 +0530

    Time: 1 ms (52.72%) | Memory: 20.7 MB (37.42%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,49 +1,30 @@
-class DisjointSet{
-    public: vector<int> rank,parent;
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-        }
-    }
-    int findupar(int node){
-        if(node==parent[node]) return node;
-        return parent[node]=findupar(parent[node]);
-    }
-    void unionbyrank(int u,int v){
-        int ulp=findupar(u);
-        int vlp=findupar(v);
-        if(ulp==vlp) return;
-        if(rank[ulp]<rank[vlp]){
-            parent[ulp]=vlp;
-        }
-        else if(rank[vlp]<rank[ulp]){
-            parent[vlp]=ulp;
-        }
-        else{
-            parent[ulp]=vlp;
-            rank[vlp]++;
-        }
-    }
-};
 class Solution {
 public:
+void dfs(int node,vector<vector<int>> &adj,vector<int>& vis){
+    vis[node]=1;
+    for(auto it:adj[node]){
+        if(!vis[it]){
+            dfs(it,adj,vis);
+        }
+    }
+}
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        DisjointSet ds(n*n);
+        vector<vector<int>> adj(n+1);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                int u=i*n+j;
-                int v=j*n+i;
-                if(isConnected[i][j]==1){
-                    ds.unionbyrank(i,j);
+                if(i!=j&&isConnected[i][j]==1){
+                    adj[i].push_back(j);
                 }
             }
         }
         int cnt=0;
+        vector<int> vis(n+1,0);
         for(int i=0;i<n;i++){
-            if(ds.parent[i]==i) cnt++;
+            if(!vis[i]){
+                dfs(i,adj,vis);
+                cnt++;
+            }
         }
         return cnt;
     }
