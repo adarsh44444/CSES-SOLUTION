50458082255097933b26d5995cf44933db53fe68
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-07-26 07:38:03 +0530

    Time: 2 ms (17.75%) | Memory: 18.4 MB (35.98%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int ini=image[sr][sc];
-        queue<pair<int,pair<int,int>>> q;
-        q.push({ini,{sr,sc}});
+        int inicolor=image[sr][sc];
         int n=image.size();
         int m=image[0].size();
-        vector<vector<int>> ans(n,vector<int>(m,-1));
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        while(!q.empty()){
-            auto it=q.front();
-            int colour=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-            q.pop();
-            ans[row][col]=color;
-            for(int i=0;i<4;i++){
-                int ar=row+dx[i];
-                int ac=col+dy[i];
-                if(ar>=0&&ar<n&&ac<m&&ac>=0&&image[ar][ac]==ini&&ans[ar][ac]==-1){
-                    q.push({colour,{ar,ac}});
-                }
-            }
-        }
+        vector<vector<int>> ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(ans[i][j]==color) continue;
-                else ans[i][j]=image[i][j];
+                ans[i][j]=image[i][j];
+            }
+        }
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int dx[]={-1,0,1,0};
+        int dy[]={0,1,0,-1};
+        ans[sr][sc]=color;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int adjrow=row+dx[i];
+                int adjcol=col+dy[i];
+                if(adjrow>=0&&adjrow<n&&adjcol>=0&&adjcol<m&&image[adjrow][adjcol]==inicolor&&ans[adjrow][adjcol]!=color){
+                    q.push({adjrow,adjcol});
+                    ans[adjrow][adjcol]=color;
+                }
             }
         }
         return ans;
