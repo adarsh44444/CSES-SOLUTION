f479c859dcc0ab7c68e3e0bfc74ad8eefc006e1c
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2025-10-15 11:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (85.41%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,15 +1,51 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int longestSubstring(string s, int k) {
-        vector<int> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
-        for (int i = 0; i < s.size(); ++i) {
-            if (freq[s[i] - 'a'] < k) {
-                int left = longestSubstring(s.substr(0, i), k);
-                int right = longestSubstring(s.substr(i + 1), k);
-                return max(left, right);
+    int longestSubstring(std::string s, int k) {
+        return longestSubstringRecursive(s, 0, s.length(), k);
+    }
+
+private:
+    int longestSubstringRecursive(const std::string& s, int start, int end, int k) {
+        if (end - start < k) {
+            return 0;
+        }
+
+        std::vector<int> freq(26, 0);
+        for (int i = start; i < end; ++i) {
+            freq[s[i] - 'a']++;
+        }
+
+        int splitIndex = -1;
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] > 0 && freq[i] < k) {
+                for (int j = start; j < end; ++j) {
+                    if (s[j] - 'a' == i) {
+                        splitIndex = j;
+                        break;
+                    }
+                }
+                break;
             }
         }
-        return s.size();
+
+        if (splitIndex == -1) {
+            return end - start;
+        }
+
+        int maxLen = 0;
+        int currentStart = start;
+        for (int i = start; i < end; ++i) {
+            if (freq[s[i] - 'a'] < k) {
+                maxLen = std::max(maxLen, longestSubstringRecursive(s, currentStart, i, k));
+                currentStart = i + 1;
+            }
+        }
+        maxLen = std::max(maxLen, longestSubstringRecursive(s, currentStart, end, k));
+
+        return maxLen;
     }
 };
