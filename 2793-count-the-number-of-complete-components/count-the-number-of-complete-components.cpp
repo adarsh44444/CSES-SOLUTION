5661cce330a0662d91e2ee98d13b12a1bc5e4b84
adarsh44444 <123071845+adarsh44444@users.noreply.github.com>
2026-09-14 11:22:11 +0530

    Time: 40 ms (68.06%) | Memory: 130.5 MB (56.55%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,69 +1,45 @@
-
-class DisjointSet {
-    vector<int> rank, parent, size;  // Adding size to count elements in each component
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);  // Initialize size of each component as 1
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);  // Path compression
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-};
-
 class Solution {
 public:
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& comp) {
+        vis[node] = 1;
+        comp.push_back(node);
+
+        for (int nei : adj[node]) {
+            if (!vis[nei]) {
+                dfs(nei, adj, vis, comp);
+            }
+        }
+    }
+
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        DisjointSet ds(n);
-        vector<int> degree(n, 0);  // Stores degree of each node
+        vector<vector<int>> adj(n);
 
-        for (auto &edge : edges) {
-            ds.unionByRank(edge[0], edge[1]);
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
 
-        unordered_map<int, vector<int>> componentNodes;
+        vector<int> vis(n, 0);
+        int ans = 0;
+
         for (int i = 0; i < n; i++) {
-            int root = ds.findUPar(i);
-            componentNodes[root].push_back(i);
-        }
+            if (vis[i]) continue;
 
-        int completeCount = 0;
-        for (auto &[root, nodes] : componentNodes) {
-            bool isComplete = true;
-            int componentSize = nodes.size();
-            for (int node : nodes) {
-                if (degree[node] != componentSize - 1) {
-                    isComplete = false;
+            vector<int> comp;
+            dfs(i, adj, vis, comp);
+
+            bool complete = true;
+
+            for (int node : comp) {
+                if (adj[node].size() != comp.size() - 1) {
+                    complete = false;
                     break;
                 }
             }
-            if (isComplete) completeCount++;
+
+            if (complete) ans++;
         }
 
-        return completeCount;
+        return ans;
     }
 };
